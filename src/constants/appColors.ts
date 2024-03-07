e9126db9709acaba6078db97470a8969e5b52923
thanhtu <daothanhtu1996@gmail.com>
2024-03-07 17:08:12 +0700

    first commit

---
+++ src/constants/appColors.ts
@@ -0,0 +1,6 @@
+export const appColors = {
+    primary:"#5669FF",
+    white:"#fff",
+    test:"#120D26",
+    gray:"#E5E5E5"
+}
---
+++ src/screens/auth/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text style={{justifyContent:"center",textAlign:"center",alignItems:"center",marginTop:50}}>LoginScreen</Text>
+    </View>
+  )
+}
+
+export default LoginScreen
