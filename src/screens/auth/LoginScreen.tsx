725184cb193f72222e68b2f66871a68ffffcd437
thanhtu <daothanhtu1996@gmail.com>
2024-03-20 13:00:18 +0700

    up

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,10 +1,11 @@
 import {View, Text, Button} from 'react-native';
 import React from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ButtonComponent } from '../../components';
 
 const LoginScreen = () => {
   return (
-    <View>
+    <View style={{}}>
       <Text
         style={{
           justifyContent: 'center',
@@ -14,11 +15,19 @@ const LoginScreen = () => {
         }}>
         LoginScreen
       </Text>
-      <Button title="logout" onPress={async () => await AsyncStorage.clear} />
 
       <Button
         title="login"
         onPress={async () => await AsyncStorage.setItem('assetToken', 'aaaaa')}
+      />
+      <ButtonComponent
+      text='Login'
+      onPress={()=> console.log('login')}
+      icon={
+        <View>
+          <Text>N</Text>
+          </View>
+      }
       />
     </View>
   );
